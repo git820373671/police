975f0c2e04111b3956f0ca0f95d8a3979d4986ee
[banYingGit] <[635221846@qq.com]>
2017-09-19 09:51:42 +0800

    页面提交

---
+++ resources/src/components/common/raidoBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="radio-box">
+    <mt-radio
+      align="right"
+      v-model="radioData.value"
+      :options="radioData.radioList">
+    </mt-radio>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'select-box',
+    data: function () {
+      return {}
+    },
+    props: {
+      radioData: {
+        type: Object,
+        radioList: []
+      }
+    }
+  }
+</script>
